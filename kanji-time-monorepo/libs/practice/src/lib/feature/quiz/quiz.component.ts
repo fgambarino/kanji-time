93d7da4c66f1cc8e64b82011563a8a48a46a4e99
Federico Gambarino <federico.gambarino@deltatre.it>
2021-02-16 16:04:45 +0100

    practice feature mvp

--- kanji-time-monorepo/libs/practice/src/lib/feature/quiz/quiz.component.ts
+++ kanji-time-monorepo/libs/practice/src/lib/feature/quiz/quiz.component.ts
@@ -2,7 +2,7 @@ import { Question } from './../../domain/question.model';
 import { PracticeService } from './../services/practice.service';
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { BehaviorSubject, Subject, zip } from 'rxjs';
-import { delay, share, switchMap, take, takeUntil } from 'rxjs/operators';
+import { share, switchMap, takeUntil } from 'rxjs/operators';
 import { UiQuestion } from '@kanji-time-monorepo/shared';
 
 @Component({
@@ -18,6 +18,7 @@ export class QuizComponent implements OnInit, OnDestroy {
   public numberCorrect = 0;
   public totalQuestions: number;
   public sectionToShow: 'loading' | 'quiz' | 'result';
+  public resultMessage: string;
 
   private showNextQuestion$: BehaviorSubject<boolean> = new BehaviorSubject(
     true
@@ -45,15 +46,32 @@ export class QuizComponent implements OnInit, OnDestroy {
       questions$.pipe(
         switchMap((questions) => {
           return shuffleArray(questions);
-        }),
-        take(10)
+        })
       )
     )
       .pipe(takeUntil(this.destroy$))
       .subscribe({
         next: ([, kanjiQuestion]) => this._setKanjiForQuestion(kanjiQuestion),
-        complete: () => (this.sectionToShow = 'result'),
+        complete: () => {
+          this.sectionToShow = 'result';
+          this.resultMessage = this.getResultMessage();
+        },
       });
+  }
+
+  getResultMessage(): string {
+    const percentage = this.numberCorrect / this.numberAnswered;
+    if (percentage < 0.4) {
+      return 'You need to study and practice more! 😬';
+    } else if (percentage < 0.6) {
+      return "Good, but it's not enough! 😝";
+    } else if (percentage < 0.8) {
+      return "Great! You're almost there! 💪";
+    } else if (percentage < 1) {
+      return 'Wonderful! You are just one step far from perfection! 😎';
+    } else {
+      return 'Flawless victory! 💯';
+    }
   }
 
   onAnswer(correct: boolean) {
@@ -61,7 +79,6 @@ export class QuizComponent implements OnInit, OnDestroy {
     if (correct) {
       ++this.numberCorrect;
     }
-    console.log(this.numberCorrect / this.numberAnswered);
     this.showNextQuestion$.next(true);
   }
 
